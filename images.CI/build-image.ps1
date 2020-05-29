d0a0a68cb4962bb6006146b072a039fb7e58484d
osi-tng <tng@osisoft.com>
2020-05-29 15:52:12 -0700

    Update build-image.ps1

--- images.CI/build-image.ps1
+++ images.CI/build-image.ps1
@@ -51,9 +51,6 @@ packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
                 -var "subscription_id=$SubscriptionId" `
                 -var "temp_resource_group_name=$TempResourceGroupName" `
                 -var "tenant_id=$TenantId" `
-                `#-var "virtual_network_name=$VirtualNetworkName" `
-                `#-var "virtual_network_resource_group_name=$VirtualNetworkRG" `
-                `#-var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
                 -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
                 $TemplatePath `
         | Where-Object {
